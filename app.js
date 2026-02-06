fa3bccc26102794c0106facc41bab54357102609
Your Name <youremail@exaple.com>
2026-02-06 12:24:20 +0000

    0206

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+// Import Libraries and Setup
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+//// REMOVE IF YOU PUT ON RENDER //////
+import open, { openApp, apps } from "open"; //only needed for a simple development tool remove if hosting online see above
+//// REMOVE IF YOU PUT ON RENDER //////
 
 const app = express();
-const PORT = 3000;
+const server = http.createServer(app); //socket.io needs an http server
+const io = new Server(server);
+const port = process.env.PORT || 3500;
 
-app.use(express.static(path.join(__dirname, "public")));
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+//Tell our Node.js Server to host our P5.JS sketch from the public folder
+app.use(express.static("public"));
+
+// Setup Our Node.js server to listen to connections
+server.listen(port, () => {
+  console.log("listening on: " + port);
+});
+
+//// REMOVE IF YOU PUT ON RENDER //////
+//open in browser: dev environment only!
+await open(`http://localhost:${port}`); //opens in your default browser
+//// REMOVE IF YOU PUT ON RENDER //////
+
+// Callback function for when our P5.JS sketch connects
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  //0129
+  socket.broadcast.emit("drawing", data);
+
+  console.log(data); // print to console
+
+  ////IMPLEMENT MULTI-USER DRAWING////
+
+  ////IMPLEMENT MULTI-USER DRAWING////
 });
