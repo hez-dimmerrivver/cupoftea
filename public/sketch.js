d4bbd97f0260aff4068c9da2d91b5ad5da359d26
Your Name <youremail@exaple.com>
2026-02-06 18:45:33 +0000

    0206_try1

--- public/sketch.js
+++ public/sketch.js
@@ -47,14 +47,12 @@ socket.on("allSelections", (selections) => {
   }
 });
 
-let centerX, centerY, radius;
+const centerX = 400;
+const centerY = 400;
+const radius = 300;
 
 function setup() {
   const canvas = createCanvas(windowWidth, windowHeight);
-  centerX = width / 2;
-  centerY = 400;
-  radius = 400;
-
   canvas.parent("sketch-container");
 
   //grab the log element from the DOM
@@ -127,15 +125,18 @@ function draw() {
   Engine.update(engine);
 
   //draw cup
+  push();
+  translate(width / 2, 0);
   fill(240);
   noStroke();
   strokeWeight(3);
   arc(centerX, centerY, radius * 2, radius * 2, 0, PI);
+  pop();
 
   //UI
   noStroke();
   fill(0);
-  textSize(32);
+  textSize(22);
   textAlign(LEFT);
   text("My Cup of Tea", 20, 40);
   textSize(16);
@@ -144,9 +145,9 @@ function draw() {
 
   // Show participant count on the canvas
   fill(0);
-  textSize(16);
-  textAlign(RIGTH);
-  text(`Participants: ${participantCount}`, width - 20, 100);
+  textSize(18);
+  textAlign(LEFT);
+  text(`Participants: ${participantCount}`, 20, 100);
 
   //show my choice
   for (let inst of genreInstances) {
